60a4b758c595f50b9ef2e1a06f0e030a5ef876f0
Mxllo <lucasmellog@gmail.com>
2021-02-24 18:23:35 -0300

    Ambiente de testes preparado

---
+++ api/src/__tests__/User.test.ts
@@ -0,0 +1,30 @@
+import { response } from "express";
+import request from "supertest";
+import { app } from "../app";
+import createConnection from "../database"
+
+describe("Users", () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it("Should be able to create a new user", async () => {
+        const response = await request(app).post("/users")
+        .send({
+            email: "test_eabil@gamil.xom",
+            name: "Test Name",
+        });
+        expect(response.status).toBe(200);
+    });
+
+    it("Should not be able to create a new user", async () => {
+        const response = await request(app).post("/users")
+        .send({
+            email: "test_eabil@gamil.xom",
+            name: "Test Name",
+        });
+        expect(response.status).toBe(400);
+    });
+    
+});
--- api/src/controller/UserController.ts
+++ api/src/controller/UserController.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
-import { User } from "../models/User";
 import { UserRepository } from "../repositories/UserRepository";
-import { UserService } from "../services/UserService";
+
 
 class UserController{
     async create(req: Request, res: Response){
@@ -11,10 +10,10 @@ class UserController{
         if(await usersRepository.findOne({email})){
             return res.status(400).json({"error": "User already exists"});
         }
-
+        
         const user = usersRepository.create(req.body);
-        await usersRepository.save(user)
-        return res.json(user).status(200);
+        await usersRepository.save(user);
+        return res.json(user).status(201);
     }
 
     async findAll(req: Request, res: Response){
